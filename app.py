fb8475561a84d2ef95ab737764e968f8ae432863
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2019-11-11 07:31:47 +0000

    cors add

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 import os 
 
 UPLOAD_FOLDER = os.getcwd()
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
